a3d478cecd7e3fcad1953bc096536b766ebb9a19
YosefAhmed <yos7efahmed@gmail.com>
2018-12-03 23:56:49 +0200

    V1 (initial design)

--- Hangman/Game_Form.Designer.cs
+++ Hangman/Game_Form.Designer.cs
@@ -28,23 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Pic_panel = new System.Windows.Forms.Panel();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Game_Form));
+            this.masn2a = new System.Windows.Forms.Panel();
+            this.regl_shemal = new System.Windows.Forms.Panel();
+            this.Eed_shemal = new System.Windows.Forms.Panel();
+            this.regl_ymeen = new System.Windows.Forms.Panel();
+            this.Eed_ymeen = new System.Windows.Forms.Panel();
+            this.gesm = new System.Windows.Forms.Panel();
+            this.ra2s = new System.Windows.Forms.Panel();
             this.def_txt = new System.Windows.Forms.Label();
             this.hint_txt = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.keyboard1 = new Hangman.Keyboard();
-            this.label2 = new System.Windows.Forms.Label();
-            this.Pic_panel.SuspendLayout();
+            this.masn2a.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Pic_panel
+            // masn2a
             // 
-            this.Pic_panel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.Pic_panel.Controls.Add(this.label2);
-            this.Pic_panel.Location = new System.Drawing.Point(444, 125);
-            this.Pic_panel.Name = "Pic_panel";
-            this.Pic_panel.Size = new System.Drawing.Size(198, 251);
-            this.Pic_panel.TabIndex = 1;
+            this.masn2a.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("masn2a.BackgroundImage")));
+            this.masn2a.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.masn2a.Controls.Add(this.regl_shemal);
+            this.masn2a.Controls.Add(this.Eed_shemal);
+            this.masn2a.Controls.Add(this.regl_ymeen);
+            this.masn2a.Controls.Add(this.Eed_ymeen);
+            this.masn2a.Controls.Add(this.gesm);
+            this.masn2a.Controls.Add(this.ra2s);
+            this.masn2a.Location = new System.Drawing.Point(444, 125);
+            this.masn2a.Name = "masn2a";
+            this.masn2a.Size = new System.Drawing.Size(198, 251);
+            this.masn2a.TabIndex = 1;
+            // 
+            // regl_shemal
+            // 
+            this.regl_shemal.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("regl_shemal.BackgroundImage")));
+            this.regl_shemal.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.regl_shemal.Location = new System.Drawing.Point(144, 126);
+            this.regl_shemal.Name = "regl_shemal";
+            this.regl_shemal.Size = new System.Drawing.Size(24, 25);
+            this.regl_shemal.TabIndex = 7;
+            // 
+            // Eed_shemal
+            // 
+            this.Eed_shemal.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Eed_shemal.BackgroundImage")));
+            this.Eed_shemal.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Eed_shemal.Location = new System.Drawing.Point(146, 101);
+            this.Eed_shemal.Name = "Eed_shemal";
+            this.Eed_shemal.Size = new System.Drawing.Size(24, 25);
+            this.Eed_shemal.TabIndex = 6;
+            // 
+            // regl_ymeen
+            // 
+            this.regl_ymeen.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("regl_ymeen.BackgroundImage")));
+            this.regl_ymeen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.regl_ymeen.Location = new System.Drawing.Point(115, 127);
+            this.regl_ymeen.Name = "regl_ymeen";
+            this.regl_ymeen.Size = new System.Drawing.Size(24, 25);
+            this.regl_ymeen.TabIndex = 5;
+            // 
+            // Eed_ymeen
+            // 
+            this.Eed_ymeen.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Eed_ymeen.BackgroundImage")));
+            this.Eed_ymeen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Eed_ymeen.Location = new System.Drawing.Point(113, 101);
+            this.Eed_ymeen.Name = "Eed_ymeen";
+            this.Eed_ymeen.Size = new System.Drawing.Size(24, 25);
+            this.Eed_ymeen.TabIndex = 4;
+            // 
+            // gesm
+            // 
+            this.gesm.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("gesm.BackgroundImage")));
+            this.gesm.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.gesm.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.gesm.Location = new System.Drawing.Point(124, 102);
+            this.gesm.Name = "gesm";
+            this.gesm.Size = new System.Drawing.Size(37, 37);
+            this.gesm.TabIndex = 3;
+            // 
+            // ra2s
+            // 
+            this.ra2s.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ra2s.BackgroundImage")));
+            this.ra2s.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ra2s.Location = new System.Drawing.Point(123, 69);
+            this.ra2s.Name = "ra2s";
+            this.ra2s.Size = new System.Drawing.Size(37, 37);
+            this.ra2s.TabIndex = 2;
             // 
             // def_txt
             // 
@@ -84,17 +151,6 @@
             this.keyboard1.Size = new System.Drawing.Size(253, 229);
             this.keyboard1.TabIndex = 0;
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(43, 108);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(117, 20);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "هنا صورة حد مشنوق";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
             // Game_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -104,12 +160,11 @@
             this.Controls.Add(this.keyboard1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.def_txt);
-            this.Controls.Add(this.Pic_panel);
+            this.Controls.Add(this.masn2a);
             this.Name = "Game_Form";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Game";
-            this.Pic_panel.ResumeLayout(false);
-            this.Pic_panel.PerformLayout();
+            this.masn2a.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -118,10 +173,15 @@
         #endregion
 
         private Keyboard keyboard1;
-        private System.Windows.Forms.Panel Pic_panel;
+        private System.Windows.Forms.Panel masn2a;
         private System.Windows.Forms.Label def_txt;
         private System.Windows.Forms.Label hint_txt;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel regl_shemal;
+        private System.Windows.Forms.Panel Eed_shemal;
+        private System.Windows.Forms.Panel regl_ymeen;
+        private System.Windows.Forms.Panel Eed_ymeen;
+        private System.Windows.Forms.Panel gesm;
+        private System.Windows.Forms.Panel ra2s;
     }
 }
